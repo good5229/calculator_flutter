bb027ac96c74d183c036ca4daf3df1bb07b27ac4
bellhundred <good5229@naver.com>
2021-02-26 15:48:31 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String _title = 'Widget Example';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: WidgetApp(),
+    );
+  }
+}
+
+class WidgetApp extends StatefulWidget {
+  WidgetApp({Key key}) : super(key: key);
+  @override
+  _WidgetExampleState createState() => _WidgetExampleState();
+}
+
+class _WidgetExampleState extends State<WidgetApp>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar : AppBar(
+        title: Text('Widget Example'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: Text('Flutter'),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left:20, right:20),
+                child: TextField(),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: RaisedButton(onPressed: (){
+
+                }),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
